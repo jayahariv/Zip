e570091f25fc227efbe85bb54bc7749c224fc038
jayahariv <jayahariv88@gmail.com>
2020-09-18 20:28:41 -0700

    * update the iOS version and device

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # **** Update me when new Xcode versions are released! ****
-PLATFORM="platform=iOS Simulator,OS=13.0,name=iPhone 8"
+PLATFORM="platform=iOS Simulator,OS=14.0,name=iPhone 11"
 SDK="iphonesimulator"
 
 # It is pitch black.
